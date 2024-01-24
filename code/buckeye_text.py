17c8a1a41312f9c2dc3a9f976caaf2a682b2d91f
ansost <anna.stein@hhu.de>
2024-01-24 09:12:32 +0100

    pep8 my script

--- code/buckeye_text.py
+++ code/buckeye_text.py
@@ -1,30 +1,25 @@
-#importing corpus tool
-import buckeye 
+"""Extract word lists per speaker from Buckeye corpus."""
+import buckeye
 
-# Generating corpus
-corpus = buckeye.corpus('/home/ansost/buckeye_corpus')
+if __name__ == "__main__":
+    corpus = buckeye.corpus("../data/buckeye_corpus/")
 
-#iterating over all 40 speakers form corpus
-for speaker in corpus:
-    
-    #init word_list
-    word_list = []
-    #print('speaker here!')
-    
-    #each speaker has up to 6 tracks (interviews)
-    #iterating over each interview of a speaker
-    for track in speaker:
-        
-        #iterating over each word for wach interview to append it to the dataframe
-        for word in track.words: 
-            
-            #the word attribute has two attributes: word and pause. 
-            #We dont want the pauses and they dont have an orthography attribute
-            if hasattr(word, 'orthography'): 
-                word_list.append(word.orthography)
+    # Iterating over all 40 speakers form corpus.
+    for speaker in corpus:
+        # Init word_list.
+        word_list = []
 
-    #writing the word_list to new file 
-    new_file = open('/home/ansost/buck_me/' + speaker.name + ".txt", "w")
-    for line in word_list:
-        new_file.write(line +'\n')
-    new_file.close()
+        # Each speaker has up to 6 tracks (interviews).
+        # Iterating over each interview of a speaker.
+        for track in speaker:
+            # Iterating over each word for wach interview to append it to the dataframe.
+            for word in track.words:
+                # The word attribute has two attributes: word and pause.
+                # We dont want the pauses and they dont have an orthography attribute.
+                if hasattr(word, "orthography"):
+                    word_list.append(word.orthography)
+
+        new_file = open("../data/" + speaker.name + ".txt", "w")
+        for line in word_list:
+            new_file.write(line + "\n")
+        new_file.close()
